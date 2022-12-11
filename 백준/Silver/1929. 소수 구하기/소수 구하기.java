e6c316d3ca2de1ed0c75680d45bdf1bbd52bb8ef
camembert1 <119032690+camembert1@users.noreply.github.com>
2022-12-11 19:58:11 +0900

    [Silver III] Title: 소수 구하기, Time: 660 ms, Memory: 22028 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -1,29 +1,44 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
 
 public class Main {
 
 	public static void main(String[] args) {
-
 		
-		Scanner sc = new Scanner(System.in);
-		int M = sc.nextInt();
-		int N = sc.nextInt();
-		
-		for (int i = M; i <= N; i++) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+		try {
+			st = new StringTokenizer(br.readLine());
+			int M = Integer.parseInt(st.nextToken());
+			int N = Integer.parseInt(st.nextToken());
 			
-			boolean isPrime = true;
-			
-			if(i == 1) isPrime = false;
-			for (int j = 2; j <= Math.sqrt(i); j++) {
-				if(i%j == 0) {
-					isPrime = false;
-					break;
+			for (int i = M; i <= N; i++) {
+				
+				boolean isPrime = true;
+				
+				if(i == 1) isPrime = false;
+				for (int j = 2; j <= Math.sqrt(i); j++) {
+					if(i%j == 0) {
+						isPrime = false;
+						break;
+					}
 				}
+				if (isPrime) bw.write(i+"\n");
 			}
+			bw.flush();
+			bw.close();
 			
-			if (isPrime) System.out.println(i);
-			
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
+		
+		
 
 	}
 
