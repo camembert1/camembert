daaffea8d91e648ec5984dcbf55b4f45da0290bc
camembert1 <119032690+camembert1@users.noreply.github.com>
2022-12-11 14:12:54 +0900

    [Silver IV] Title: 괄호, Time: 236 ms, Memory: 17860 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -3,14 +3,13 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		
+
 		Scanner sc = new Scanner(System.in);
 		int num = sc.nextInt();
 		
 		for (int i = 0; i < num; i++) {
 			String vps = sc.next();
 			System.out.println(findVPS(vps));
-			
 		}
 	}
 
@@ -18,7 +17,7 @@ public class Main {
 		while (vps.contains("()")) {
 			vps = vps.replace("()", "");
 		}
-		if (vps.equals("")) return "YES";
-		else return "NO";
+		return (vps.equals(""))? "YES": "NO";
 	}
+	
 }
