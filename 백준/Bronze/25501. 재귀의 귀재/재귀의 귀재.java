2f11d348f6b4abf6370d4b1dfd44bab803863b62
camembert1 <119032690+camembert1@users.noreply.github.com>
2022-12-18 19:16:57 +0900

    [Bronze II] Title: 재귀의 귀재, Time: 764 ms, Memory: 44436 KB -BaekjoonHub

---
+++ 백준/Bronze/25501. 재귀의 귀재/재귀의 귀재.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main{
+	
+	static int cnt = 0;
+	
+    public static int recursion(String s, int l, int r){
+    	cnt++;
+    	if(l >= r) return 1;
+        else if(s.charAt(l) != s.charAt(r)) return 0;
+        else return recursion(s, l+1, r-1);
+    }
+    
+    public static int isPalindrome(String s){
+        return recursion(s, 0, s.length()-1);
+    }
+    
+    public static void main(String[] args){
+   
+    	Scanner sc = new Scanner(System.in);
+    	int T = sc.nextInt();
+    	for (int i = 0; i < T; i++) {
+			String S = sc.next();
+			System.out.printf("%d %d%n", isPalindrome(S), cnt);
+			cnt = 0;
+		}
+    	
+    }
+    
+}
