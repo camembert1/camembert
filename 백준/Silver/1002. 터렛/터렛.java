d21df1eac526637b371c5dc4e352ad8604c590fd
Yujeong Lee <119032690+camembert1@users.noreply.github.com>
2023-01-16 02:46:34 +0900

    [Silver III] Title: 터렛, Time: 276 ms, Memory: 19036 KB -BaekjoonHub

---
+++ 백준/Silver/1002. 터렛/터렛.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int result = 0;
+		int T = sc.nextInt();
+		for (int i = 0; i < T; i++) {
+			int x1 = sc.nextInt(), y1 = sc.nextInt(), r1 = sc.nextInt(),
+				x2 = sc.nextInt(), y2 = sc.nextInt(), r2 = sc.nextInt();
+			
+			int dif = Math.abs(r2-r1);
+			int sum = r2 + r1;
+			double d = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+			
+			if (dif < d && d < sum) result = 2;
+			if (dif == d || sum == d) result = 1;
+			if (d < dif || sum < d) result = 0;
+			if (dif == 0 && x2 == x1 && y2 == y1) result = -1;
+			
+			System.out.println(result);
+		}
+
+	}
+}
